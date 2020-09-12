117085979d5f6afd6ee630184408238845a90888
Hanna Rosenfeld <thesoftestbrain@gmail.com>
2020-09-12 16:23:55 -0400

    Update bio.

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,26 +1,11 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
       site {
         siteMetadata {
           author
@@ -35,20 +20,12 @@ const Bio = () => {
   const { author, social } = data.site.siteMetadata
   return (
     <div className="flex mb-20">
-      <Image
-        className="mr-4 mb-0 rounded-full"
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
-        style={{
-          minWidth: 50,
-        }}
-      />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> who lives and works in Philly
+        building useful things.
         {` `}
         <a
-          className="text-blue-600"
+          className="text-pink-600"
           href={`https://twitter.com/${social.twitter}`}
         >
           You should follow him on Twitter
